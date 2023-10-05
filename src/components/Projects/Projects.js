a4076ec1f5aba66573f81c005efe8906c8f40625
Naveen297 <naveenmalhotra148@gmail.com>
2023-10-05 22:44:31 +0530

    commitNetlifytest

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -18,7 +18,7 @@ function Projects() {
           My Recent <strong className="purple">Works </strong>
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few projects Please like them!.
+          Here are a few projects I've worked on recently.
         </p>
 
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
