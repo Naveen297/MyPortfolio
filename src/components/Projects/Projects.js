6ee7d71f8cf54036b13638c84dadc5a7ee2fbeb5
Naveen297 <naveenmalhotra148@gmail.com>
2023-10-05 22:57:40 +0530

    finaltest

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -18,7 +18,7 @@ function Projects() {
           My Recent <strong className="purple">Works </strong>
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
+          Here are a few projects please like them.
         </p>
 
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
