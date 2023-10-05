2b2a00df1713f0f8baa86a9b8650c8db46361657
Naveen297 <naveenmalhotra148@gmail.com>
2023-10-05 21:56:08 +0530

    commitnewabhi

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -18,7 +18,8 @@ function Projects() {
           My Recent <strong className="purple">Works </strong>
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently. hope you like them
+          Here are a few projects I've worked on recently. Please appriciate
+          them
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
