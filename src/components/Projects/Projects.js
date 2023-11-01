fb201c44a172fb5b03a84e6f426a32967df007ec
Naveen297 <naveenmalhotra148@gmail.com>
2023-11-01 15:28:01 +0530

    committoday

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -15,7 +15,7 @@ function Projects() {
       <Particle />
       <Container>
         <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
+          My Recent <strong className="purple">Project Works </strong>
         </h1>
         <p style={{ color: "white" }}>
           Here are a few projects that I worked upon.
